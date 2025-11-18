cb87fdc4ac28f235cc9841fa1983c3efb28af19a
Ankur Jat <ankurandroid2908@gmail.com>
2025-11-18 22:17:40 +0100

    added support for type command

--- app/main.py
+++ app/main.py
@@ -9,8 +9,15 @@ def main():
             break
         if command.startswith("echo "):
             sys.stdout.write(command[5:] + "\n")
-        else:
-            sys.stdout.write(f"{command}: command not found\n")
+            continue
+        if command.startswith("type "):
+            type_command = command[5:]
+            if type_command in ["echo", "exit", "type"]:
+                sys.stdout.write(f"{type_command} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{type_command}: not found\n")
+            continue
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
